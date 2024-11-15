d23ee433f07d2f601d4f923da5cba6bd67bf06e6
mayrabevacqua <mayrabevacqua@gmail.com>
2024-11-15 16:27:48 -0300

    Clase jugador y clase juego

--- JuegoCasino.ts
+++ JuegoCasino.ts
@@ -1,1 +1,28 @@
-export class JuegoCasino {}
+export abstract class JuegoCasino {
+    private nombre: string;
+    private apuesta: number;
+    private miniInstruccion: string;
+  
+    constructor(nombre: string, apuesta: number, miniInstruccion: string) {
+      this.nombre = nombre;
+      this.apuesta = apuesta;
+      this.miniInstruccion = miniInstruccion;
+    }
+  
+    abstract jugar(): void;
+  
+    abstract resultado(): void;
+  
+    public getNombre(): string {
+      return this.nombre;
+    }
+  
+    public getApuesta(): number {
+      return this.apuesta;
+    }
+  
+    public getMiniInstruccion(): string {
+      return this.miniInstruccion;
+    }
+  }
+  
